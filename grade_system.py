5a1e32218e85d3c318fe690c27237d5b8dc938be
joansampa837 <joansampa83@gmail.com>
2026-02-18 10:56:29 +0200

    added validation

--- grade_system.py
+++ grade_system.py
@@ -3,8 +3,21 @@
 def get_marks():
     marks = []
     for i in range(5):
+<<<<<<< HEAD
         mark = float(input(f"Enter mark {i+1}: "))
         marks.append(mark)
+=======
+        while True:
+            try:
+                mark = float(input(f"Enter mark {i+1} (0-100): "))
+                if 0 <= mark <= 100:
+                    marks.append(mark)
+                    break
+                else:
+                    print("Mark must be 0-100")
+            except:
+                print("Invalid input")
+>>>>>>> f37a841 (addedvaridation)
     return marks
 
 def calculate_average(marks):
@@ -25,3 +38,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
